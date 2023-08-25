97d86f23f65a49b7b968b5a51dbe1bb01304f38b
mbithuka wa safari <79813370+mbithuka@users.noreply.github.com>
2023-08-25 14:31:09 +0300

    Create validate.py
    overview,

---
+++ validate.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import time
+import random
+
+def validName(df):
+  df = pd.read_csv(df)
+  m =0
+  for _ in range(len(df)):
+    m+=1
+    wikipedia.summary(df.ScientificName[m], sentences = 2)
+    time.sleep(random.choice(range(1,10))
+
+validName(df ='')
