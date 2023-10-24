75aa30e5712ea5a39fa2831ff56c751f46ef7dff
mbithuka wa safari <79813370+mbithuka@users.noreply.github.com>
2023-10-24 15:06:18 +0300

    Update validate.py
    continues from where connection was lost

--- validate.py
+++ validate.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import time
 import random
+import wikipedia
 
 def validName(df):
-  df = pd.read_csv(df)
-  m =0
-  for _ in range(len(df)):
-    m+=1
-    wikipedia.summary(df.ScientificName[m], sentences = 2)
-    time.sleep(random.choice(range(1,10))
-
-validName(df ='')
+	df = pd.read_csv(df)
+	m = 0
+	n = 1
+	for _ in range(len(df)):
+		try:
+			print("Processing ...", df.ScientificName[m])
+			print(wikipedia.summary(df.ScientificName[m],sentences=2))
+			time.sleep(1)
+		except:
+			print("Connection lost ..., trying again")
+			continue
+		finally:
+			m = n
+			n+=1
+        
+validName(df = '')
